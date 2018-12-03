9a0ed1ee33637c3f5117f13a3ce9eea2d1d26e28
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2018-12-03 14:50:14 +0000

    Add Gradle project

---
+++ src/test/java/uk/co/automatictester/gradle/sample/SampleTest.java
@@ -0,0 +1,18 @@
+package uk.co.automatictester.gradle.sample;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    public void assertSomething() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void assertSomethingElse() {
+        assertTrue(true);
+    }
+}
